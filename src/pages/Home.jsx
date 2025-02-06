40ca675257b878ac8422d07c9920930c3ea8a10e
Ram KUMAR <ramkumar18092005@gmail.com>
2025-02-07 01:21:32 +0530

    change in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,22 +16,21 @@ function Home() {
             }
         })
     }, [])
-    // if (posts.length === 0) {
-    //     return (
-    //         loading?<Loader/>:
-    //         <div className="w-full py-8 mt-4 text-center h-screen">
-    //             <Container>
-    //                 <div className="flex flex-wrap">
-    //                     <div className="p-2 w-full">
-    //                         <h1 className="text-2xl font-bold hover:text-gray-500">
-    //                             Login to read posts
-    //                         </h1>
-    //                     </div>
-    //                 </div>
-    //             </Container>
-    //         </div>
-    //     )
-    // }
+    if (posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center h-screen">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
     return (
         loading?<Loader/>:
         <div className='w-full py-8'>
