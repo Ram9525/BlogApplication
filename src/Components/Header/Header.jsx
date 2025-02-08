9e34a4d23be99eae6073a36f5b8c7103b5774fa1
Ram KUMAR <ramkumar18092005@gmail.com>
2025-02-08 13:25:40 +0530

    header responsive added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   ];
 
   return (
-    <header className="py-4 bg-gradient-to-r from-blue-600 to-indigo-700 shadow-lg">
+    <header className="bg-gradient-to-r from-blue-600 to-indigo-700 py-4 shadow-lg">
       <Container>
         <nav className="flex items-center justify-between">
           {/* Logo */}
@@ -27,19 +27,12 @@ const Header = () => {
           </Link>
 
           {/* Mobile Menu Button */}
-          <button
-            className="md:hidden text-white"
-            onClick={() => setIsOpen(!isOpen)}
-          >
+          <button className="md:hidden text-white" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? <X size={28} /> : <Menu size={28} />}
           </button>
 
           {/* Navigation */}
-          <ul
-            className={`absolute md:static top-16 left-0 w-full bg-blue-600 md:bg-transparent p-5 md:p-0 space-y-4 md:space-y-0 md:flex md:items-center md:space-x-6 transition-all duration-300 flex items-center justify-between md:justify-end gap-10 z-50 ${
-              isOpen ? "block" : "hidden"
-            } md:block`}
-          >
+          <ul className={`absolute md:static top-16 left-0 w-full bg-blue-600 md:bg-transparent p-5 md:p-0 space-y-4 md:space-y-0 flex flex-col md:flex-row md:items-center md:space-x-6 transition-all duration-300 items-center justify-center gap-4 z-50 ${isOpen ? "flex" : "hidden"} md:flex`}>
             {navItems.map(
               (item) =>
                 item.active && (
@@ -49,7 +42,7 @@ const Header = () => {
                         navigate(item.slug);
                         setIsOpen(false);
                       }}
-                      className="block px-5 py-2 text-white text-center font-semibold rounded-lg transition-all duration-300 hover:bg-white hover:text-blue-600 md:inline cursor-pointer"
+                      className="block px-5 py-2 text-white text-center font-semibold rounded-lg transition-all duration-300 hover:bg-white hover:text-blue-600 cursor-pointer"
                     >
                       {item.name}
                     </button>
